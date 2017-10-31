5f2638d833d0bc0f66d8336844246080910e3cd2
Joabson Borges <joabson.borges@planejamento.gov.br>
2017-10-31 12:15:23 -0200

    Alteração corrindo erros no arquivos

--- src/mpdg/govbr/faleconosco/browser/mensagemaddview.py
+++ src/mpdg/govbr/faleconosco/browser/mensagemaddview.py
@@ -39,29 +39,27 @@ class MensagemAddView(grok.View):
         }
 
     def render(self, **kwargs):
-    """
-    Este metodo tem as funcionalidades de 
-    responder, encaminhar e resgatar.
-    Só quem pode responder de fato é o Adminitrador do fale conosco,
-    os demais usuários pode apenas encaminhar a resposta para o Administrador,
-    para que ele encaminhe de volta para o usuário final (remetente).
-    O encaminhamento é feito sempre pelo usuário logado, seja ele Administrador
-    ou Responsável pela mensagem.
-    O responsável pela mensagem pode encaminhar para outros usuários do Fale Conosco
-    Podendo também resgatar a mensagem, caso tenha cometido algum engano.
-    """
+    # Este metodo tem as funcionalidades de 
+    # responder, encaminhar e resgatar.
+    # Só quem pode responder de fato é o Adminitrador do fale conosco,
+    # os demais usuários pode apenas encaminhar a resposta para o Administrador,
+    # para que ele encaminhe de volta para o usuário final (remetente).
+    # O encaminhamento é feito sempre pelo usuário logado, seja ele Administrador
+    # ou Responsável pela mensagem.
+    # O responsável pela mensagem pode encaminhar para outros usuários do Fale Conosco
+    # Podendo também resgatar a mensagem, caso tenha cometido algum engano.
 
-        ucatalog       = getToolByName(self.context, 'uid_catalog')
-        wtool          = getToolByName(self.context, 'portal_workflow')
-        mtool          = getToolByName(self.context, 'portal_membership')
-        pt             = getToolByName(self.context, 'portal_types')
-        request        = self.context.REQUEST
-        status_message = IStatusMessage(request)
-        form           = self._get_form_vars(request.form)
-        uids           = form.get('uids', None)
-        acao           = form.get('estado', None)
-        mensagem       = form.get('mensagem', None)
-        userlogged     = api.user.get_current().id
+        ucatalog=getToolByName(self.context, 'uid_catalog')
+        wtool=getToolByName(self.context, 'portal_workflow')
+        mtool=getToolByName(self.context, 'portal_membership')
+        pt=getToolByName(self.context, 'portal_types')
+        request=self.context.REQUEST
+        status_message=IStatusMessage(request)
+        form=self._get_form_vars(request.form)
+        uids=form.get('uids', None)
+        acao=form.get('estado', None)
+        mensagem=form.get('mensagem', None)
+        userlogged=api.user.get_current().id
 
 
         if uids:
@@ -72,16 +70,16 @@ class MensagemAddView(grok.View):
                 obj  = fale.getObject()
                 # cria a mensagem
                 # TODO: criar metadados no catalog
-                nome        = obj.getNome()
-                assunto     = self.get_assunto(fale)
-                email       = obj.getEmail()
-                responsavel = obj.getResponsavel()
+                nome=obj.getNome()
+                assunto=self.get_assunto(fale)
+                email=obj.getEmail()
+                responsavel=obj.getResponsavel()
 
                 id = idnormalizer.normalize(nome) + \
                     '-' + str(datetime.now().microsecond)
 
-                type_info = pt.getTypeInfo('Mensagem')
-                item      = type_info._constructInstance(obj, id)
+                type_info=pt.getTypeInfo('Mensagem')
+                item=type_info._constructInstance(obj, id)
                 item.setTitle(nome)
                 item.setNome(nome)
                 item.setEmail(email)
@@ -94,13 +92,12 @@ class MensagemAddView(grok.View):
                 wtool.doActionFor(obj, acao)
                 wtool.doActionFor(item, acao)
 
-                assunto = 'Fale conosco: resposta'
-                endereco = email
-                mensagem_mail = prepare_email_message(mensagem, html=True)
+                assunto='Fale conosco: resposta'
+                endereco=email
+                mensagem_mail=prepare_email_message(mensagem, html=True)
                 simple_send_mail(mensagem_mail, endereco, assunto)
 
-            status_message.add(u"Mensagens respondidas com sucesso!",
-                               type=u"info")
+            status_message.add(u"Mensagens respondidas com sucesso!", type=u"info")
 
             contextURL = self.context.absolute_url() + '/@@fale-conosco-admin'
             return self.request.response.redirect(contextURL)
@@ -111,11 +108,11 @@ class MensagemAddView(grok.View):
             if not uid:
                 uid = request.form.get('msg')
 
-            nome        = request.form.get('nome', None)
-            email       = request.form.get('email', None)
-            assunto     = request.form.get('assunto', None)
-            userid      = request.form.get('userid', None)
-            responsavel = request.form.get('responsavel', None)
+            nome=request.form.get('nome', None)
+            email=request.form.get('email', None)
+            assunto=request.form.get('assunto', None)
+            userid=request.form.get('userid', None)
+            responsavel=request.form.get('responsavel', None)
             id = idnormalizer.normalize(nome) + \
                 '-' + str(datetime.now().microsecond)
 
@@ -124,24 +121,24 @@ class MensagemAddView(grok.View):
 
             fale = ucatalog(UID=uid)[0].getObject()
 
-            if acao == 'resgatar':
-                member      = mtool.getAuthenticatedMember()
-                responsavel = member.getId()
-                email       = member.getProperty('email')
-                assunto     = fale.getAssunto()
+            if acao=='resgatar':
+                member=mtool.getAuthenticatedMember()
+                responsavel=member.getId()
+                email=member.getProperty('email')
+                assunto=fale.getAssunto()
                 fale.setResponsavel(userlogged)
                 fale.reindexObject()
 
             # apos criar e alterar o estado do workflow,
             # seta o responsavel no fale
-            if acao == 'encaminhar':
-                userid      = request.form.get('userid', None)
+            if acao=='encaminhar':
+                userid=request.form.get('userid', None)
                 fale.setResponsavel(userid)
                 fale.reindexObject()
 
             # cria a mensagem
-            type_info = pt.getTypeInfo('Mensagem')
-            item      = type_info._constructInstance(fale, id)
+            type_info=pt.getTypeInfo('Mensagem')
+            item=type_info._constructInstance(fale, id)
             item.setTitle(nome)
             item.setNome(userid)
             item.setEmail(email)
@@ -156,27 +153,27 @@ class MensagemAddView(grok.View):
             wtool.doActionFor(fale, acao)
             wtool.doActionFor(item, acao)
 
-            assunto       = 'Fale conosco'
-            endereco      = email
-            mensagem_mail = prepare_email_message(mensagem, html=True)
+            assunto='Fale conosco'
+            endereco=email
+            mensagem_mail=prepare_email_message(mensagem, html=True)
             simple_send_mail(mensagem_mail, endereco, assunto)
 
             status_message.add(u"Alteração realizada com sucesso!", type=u"info")
 
-            contextURL = self.context.absolute_url() + \
+            contextURL=self.context.absolute_url() + \
                 '/@@fale-conosco-admin?msg=' + uid
             return self.request.response.redirect(contextURL)
 
     def get_assunto(self, conteudo):
         # metodo para buscar o titulo do assunto
 
-        factory = getUtility(IVocabularyFactory, u'mpdg.govbr.faleconosco.Assuntos')
-        vocab   = factory(self.context)
-        termo   = conteudo.getObject().getAssunto()
+        factory=getUtility(IVocabularyFactory, u'mpdg.govbr.faleconosco.Assuntos')
+        vocab=factory(self.context)
+        termo=conteudo.getObject().getAssunto()
 
         try:
 
-            assunto = vocab.getTerm(termo)
+            assunto=vocab.getTerm(termo)
             return assunto.title
 
         except LookupError:
@@ -184,13 +181,13 @@ class MensagemAddView(grok.View):
 
     def _back_to_admin(self):
 
-        p_url  = api.portal.get().absolute_url()
-        target = '{0}/@@fale-conosco-admin'.format(p_url)
+        p_url=api.portal.get().absolute_url()
+        target='{0}/@@fale-conosco-admin'.format(p_url)
 
         return self.request.response.redirect(target)
 
     def message(self, mensagem):
 
-        messages = IStatusMessage(self.request)
+        messages=IStatusMessage(self.request)
         messages.add(mensagem, type='info')
         return
